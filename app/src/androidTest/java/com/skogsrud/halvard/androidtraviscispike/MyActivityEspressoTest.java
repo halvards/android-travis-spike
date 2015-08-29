fd3b0d46c2d4036b559f3a6ac0e22cffaadd244d
Halvard Skogsrud <halvard@skogsrud.com>
2015-08-29 17:32:45 +1000

    Fix espresso test

--- app/src/androidTest/java/com/skogsrud/halvard/androidtraviscispike/MyActivityEspressoTest.java
+++ app/src/androidTest/java/com/skogsrud/halvard/androidtraviscispike/MyActivityEspressoTest.java
@@ -1,16 +1,19 @@
 package com.skogsrud.halvard.androidtraviscispike;
 
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
+@RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MyActivityEspressoTest {
     @Rule
@@ -18,6 +21,6 @@ public class MyActivityEspressoTest {
 
     @Test
     public void testMyActivityShouldSayHelloWorld() throws Exception {
-        onView(withId(R.id.text)).check(matches(withText("Hello Espresso!")));
+        onView(withId(R.id.text)).check(matches(withText("Hello world!")));
     }
 }
