cc9c4ef4916c90a05d8f24a9647b2b64d73da472
Halvard Skogsrud <halvard@skogsrud.com>
2015-08-31 13:09:51 +1000

    Fiddly Espresso test

--- app/src/androidTest/java/com/skogsrud/halvard/androidtraviscispike/HelloWorldEspressoTest.java
+++ app/src/androidTest/java/com/skogsrud/halvard/androidtraviscispike/HelloWorldEspressoTest.java
@@ -9,7 +9,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.Espresso.openContextualActionModeOverflowMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -25,6 +30,9 @@ public class HelloWorldEspressoTest {
 
     @Test
     public void testMainActivityShouldSayHome() throws Exception {
-        onView(withId(R.id.home_text)).check(matches(withText("Home")));
+//        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        openContextualActionModeOverflowMenu();
+//        onView(withId(R.id.action_settings)).check(matches(withText("Settings")));
+        onView(withText("Settings")).perform(click());
     }
 }
