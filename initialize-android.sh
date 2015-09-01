bb7f996724b204269b729fec9ff8d202ad87bd35
Halvard Skogsrud <halvard@skogsrud.com>
2015-09-01 17:03:36 +1000

    Ensure components are installed

--- initialize-android.sh
+++ initialize-android.sh
@@ -8,38 +8,40 @@ set -e
 # Existence of this file indicates that all dependencies were previously installed, and any changes to this file will use a different filename
 INITIALIZATION_FILE="$ANDROID_HOME/.initialized-dependencies-$(git log -n 1 --format=%h -- $0)"
 
-if [ ! -e "${INITIALIZATION_FILE}" ]; then
+#if [ ! -e "${INITIALIZATION_FILE}" ]; then
   # Fetch and initialize $ANDROID_HOME
   download-android
 
+  cat $(which download-android)
+
   # List available SDK components
-  android list sdk --all --extended
+  android list sdk --all --no-ui --extended
 
   # Use the latest Android SDK tools
-  echo y | android update sdk --no-ui --filter platform-tool > /dev/null
-  echo y | android update sdk --no-ui --filter tool > /dev/null
+  echo y | android update sdk --all --no-ui --filter platform-tool > /dev/null
+  echo y | android update sdk --all --no-ui --filter tool > /dev/null
 
   # The BuildTools version used by your project
-  echo y | android update sdk --no-ui --filter build-tools-22.0.1 --all > /dev/null
+  echo y | android update sdk --all --no-ui --filter build-tools-22.0.1 > /dev/null
 
   # The SDK version used to compile your project
-  echo y | android update sdk --no-ui --filter android-22 --all > /dev/null
+  echo y | android update sdk --all --no-ui --filter android-22 > /dev/null
 
   # Install the Extra/Android Support Library
-  echo y | android update sdk --no-ui --filter extra-android-support --all > /dev/null
+  echo y | android update sdk --all --no-ui --filter extra-android-support > /dev/null
 
   # Install the Extra/Google Play Services Library
-  echo y | android update sdk --no-ui --filter extra-google-google_play_services --all > /dev/null
+  echo y | android update sdk --all --no-ui --filter extra-google-google_play_services > /dev/null
 
   # Required to use Gradle or Maven to build your android project
-  echo y | android update sdk --no-ui --filter extra-google-m2repository --all > /dev/null
-  echo y | android update sdk --no-ui --filter extra-android-m2repository --all > /dev/null
+  echo y | android update sdk --all --no-ui --filter extra-google-m2repository > /dev/null
+  echo y | android update sdk --all --no-ui --filter extra-android-m2repository > /dev/null
 
   # Specify at least one system image to run emulator tests
-  echo y | android update sdk --no-ui --filter sys-img-armeabi-v7a-addon-google_apis-google-22 --all > /dev/null
+  echo y | android update sdk --all --no-ui --filter sys-img-armeabi-v7a-addon-google_apis-google-22 > /dev/null
 
   touch "${INITIALIZATION_FILE}"
-fi
+#fi
 
 android list target
 echo no | android create avd --force --name test --target "Google Inc.:Google APIs:22" --abi "google_apis/armeabi-v7a"
