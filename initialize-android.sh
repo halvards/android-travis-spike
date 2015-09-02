110bcd970e4f9b831293a0c4cf9c6b87490af364
Halvard Skogsrud <halvard@skogsrud.com>
2015-09-02 12:15:22 +1000

    Try base KitKat system image for Snap CI

--- initialize-android.sh
+++ initialize-android.sh
@@ -26,7 +26,6 @@ INITIALIZATION_FILE="$ANDROID_HOME/.initialized-dependencies-$(git log -n 1 --fo
 
   # The SDK version used to compile your project
   echo y | android --silent update sdk --all --no-ui --filter android-22
-  echo y | android --silent update sdk --all --no-ui --filter android-19
 
   # Install the Extra/Android Support Library
   echo y | android --silent update sdk --all --no-ui --filter extra-android-support
@@ -39,22 +38,20 @@ INITIALIZATION_FILE="$ANDROID_HOME/.initialized-dependencies-$(git log -n 1 --fo
   echo y | android --silent update sdk --all --no-ui --filter extra-android-m2repository
 
   # Specify at least one system image to run emulator tests
-  #echo y | android update sdk --all --no-ui --filter addon-google_apis-google-22
-  #echo y | android update sdk --all --no-ui --filter sys-img-armeabi-v7a-addon-google_apis-google-22 # requires addon-google_apis-google-22
-  #echo y | android update sdk --all --no-ui --filter sys-img-x86_64-addon-google_apis-google-22 # requires addon-google_apis-google-22
-  #echo y | android update sdk --all --no-ui --filter addon-google_apis-google-19 # ARM System Image
-  echo y | android update sdk --all --no-ui --filter addon-google_apis_x86-google-19
+  #echo y | android --silent update sdk --all --no-ui --filter addon-google_apis-google-22
+  #echo y | android --silent update sdk --all --no-ui --filter sys-img-armeabi-v7a-addon-google_apis-google-22 # requires android-22 and addon-google_apis-google-22, includes Google APIs
+  #echo y | android --silent update sdk --all --no-ui --filter sys-img-armeabi-v7a-android-22 # requires android-22
+  echo y | android --silent update sdk --all --no-ui --filter android-19
+  #echo y | android update sdk --all --no-ui --filter addon-google_apis-google-19 # requires android-19, includes Google APIs
+  echo y | android update sdk --all --no-ui --filter sys-img-armeabi-v7a-android-19 # requires android-19
 
   touch "${INITIALIZATION_FILE}"
 #fi
 
 android list target
 #echo no | android create avd --force --name test --target "Google Inc.:Google APIs:22" --abi "google_apis/armeabi-v7a"
-echo no | android create avd --force --name test --target "Google Inc.:Google APIs (x86 System Image):19" --abi "default/x86"
+#echo no | android create avd --force --name test --target "Google Inc.:Google APIs:19" --abi "default/armeabi-v7a"
+echo no | android create avd --force --name test --target "android-19" --abi "default/armeabi-v7a"
 emulator -avd test -no-skin -no-audio -no-window &
 ./android-wait-for-emulator.sh
 adb shell input keyevent 82 &
-
-echo Starting Gradle build
-./gradlew --no-daemon clean build connectedCheck
-
